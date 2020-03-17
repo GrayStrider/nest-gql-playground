688efe2249934b574f0924589e65a5901f332227
Gray Strider <zhoga.ivan@gmail.com>
2020-03-18 03:00:45 +1100

    timeout interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
-import { AppModule } from '@/app.module'
 import { NestExpressApplication } from '@nestjs/platform-express'
-import { sig } from '@qdev/utils-ts'
 import { HttpExceptionFilter } from '@/common/filters/http-exception.filter'
+import { TimeoutInterceptor } from '@/common/interceptors/timeout.interceptor'
 import { PORT, HOST } from '@config'
+import { sig } from '@qdev/utils-ts'
+import { AppModule } from '@/app.module'
 
 
 async function bootstrap () {
 	const app = await NestFactory.create <NestExpressApplication> (AppModule)
-	app.useGlobalPipes (new ValidationPipe ())
-	app.useGlobalFilters(new HttpExceptionFilter())
+	app.useGlobalPipes (new ValidationPipe)
+	app.useGlobalFilters (new HttpExceptionFilter)
+	app.useGlobalInterceptors (new TimeoutInterceptor)
 	await app.listen (PORT)
 	sig.success (`Application is running on: http://${HOST}:${PORT}`)
 }
