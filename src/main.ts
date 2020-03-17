c0af5a8e4b84941736df681589ee6f52ddf89ddf
Gray Strider <zhoga.ivan@gmail.com>
2020-03-17 23:17:17 +1100

    config variables

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,15 @@ import { AppModule } from '@/app.module'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { sig } from '@qdev/utils-ts'
 import { HttpExceptionFilter } from '@/common/filters/http-exception.filter'
+import { PORT, HOST } from '@config'
+
 
 async function bootstrap () {
 	const app = await NestFactory.create <NestExpressApplication> (AppModule)
 	app.useGlobalPipes (new ValidationPipe ())
 	app.useGlobalFilters(new HttpExceptionFilter())
-	await app.listen (3000)
-	sig.success (`Application is running on: ${await app.getUrl ()}`)
+	await app.listen (PORT)
+	sig.success (`Application is running on: http://${HOST}:${PORT}`)
 }
 
 bootstrap ()
