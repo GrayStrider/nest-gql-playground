f252fae51d75467d71515cb21e4255ecff6f7dba
Gray Strider <zhoga.ivan@gmail.com>
2020-03-17 22:18:51 +1100

    enable global exception filter

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from '@/app.module'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { sig } from '@qdev/utils-ts'
+import { HttpExceptionFilter } from '@/common/filters/http-exception.filter'
 
 async function bootstrap () {
 	const app = await NestFactory.create <NestExpressApplication> (AppModule)
 	app.useGlobalPipes (new ValidationPipe ())
-	
+	app.useGlobalFilters(new HttpExceptionFilter())
 	await app.listen (3000)
 	sig.success (`Application is running on: ${await app.getUrl ()}`)
 }
