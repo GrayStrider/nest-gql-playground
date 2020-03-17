f22d2ce1bb93868fcb65d1232eb3f6bfca06f745
Gray Strider <zhoga.ivan@gmail.com>
2020-03-18 00:38:01 +1100

    codes

--- src/common/message.ts
+++ src/common/message.ts
@@ -1,8 +1,10 @@
 import { sig } from '@qdev/utils-ts'
 import chalk from 'chalk'
+import { elem } from 'fp-ts/lib/Array'
+import { eqNumber } from 'fp-ts/lib/Eq'
 
 const { red, green } = chalk
-
+const good = [200, 201]
 export const log = (status: number, url: string, method: string, startTime: number) =>
-	`${status === 200 ? green (status) : red (status)}: ${method} ${url} ${Date.now () - startTime}ms`
+	`${elem(eqNumber)(status, good) ? green (status) : red (status)}: ${method} ${url} ${Date.now () - startTime}ms`
 
