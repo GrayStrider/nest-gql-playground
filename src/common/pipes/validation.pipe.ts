402964a8d84ab25d00eccef06102d3ec459c4aa8
Gray Strider <zhoga.ivan@gmail.com>
2020-03-18 02:57:15 +1100

    validation pipe

--- src/common/pipes/validation.pipe.ts
+++ src/common/pipes/validation.pipe.ts
@@ -1,18 +1,21 @@
 import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform, Type } from '@nestjs/common'
 import { plainToClass } from 'class-transformer'
 import { validate } from 'class-validator'
+import { isNonEmpty } from 'fp-ts/lib/Array'
 
 @Injectable ()
-export class ValidationPipe implements PipeTransform<any> {
-	async transform (value: any, metadata: ArgumentMetadata) {
-		const { metatype } = metadata
+/**
+ * Has built-in implementation
+ */
+export class ValidationPipe_ implements PipeTransform<any> {
+	async transform (value: any, { metatype }: ArgumentMetadata) {
 		if (!metatype || !this.toValidate (metatype)) {
 			return value
 		}
 		const object = plainToClass (metatype, value)
 		const errors = await validate (object)
-		if (errors.length > 0) {
-			throw new BadRequestException ('Validation failed')
+		if (isNonEmpty(errors)) {
+			throw new BadRequestException (errors)
 		}
 		return value
 	}
