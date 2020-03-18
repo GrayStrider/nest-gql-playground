1509228af738a6107dd531a3bd4519de99caa21d
Gray Strider <zhoga.ivan@gmail.com>
2020-03-18 21:11:07 +1100

    async factory

--- src/modules/connection/connection.module.ts
+++ src/modules/connection/connection.module.ts
@@ -1,17 +1,41 @@
-import { Module } from '@nestjs/common'
+import { Module, ValueProvider, FactoryProvider, Injectable } from '@nestjs/common'
 import { connection } from '@M/connection/connection.provider'
 import { Tokens } from '@/common/constants'
 import { NODE_ENV } from '@config'
+import sleep from 'sleep-promise'
 
-const ConnectionProvider = {
+@Injectable()
+class OptionsProvider {}
+
+const AsyncConnectionFactory: FactoryProvider = {
+	provide: Tokens.ASYNC_CONNECTION,
+	useFactory: async (options: OptionsProvider) => {
+		const {data} = await sleep (100) ({ data: 'foobar' })
+		return data
+	},
+	inject: [OptionsProvider]
+}
+
+/**
+ * useValue vs useFactory
+ *
+ * useFactory takes a factory function that is expected to return the value
+ * and also can have dependencies
+ * (require instances of other providers passed as parameter)
+ *
+ * - Angular
+ */
+const ConnectionProvider: ValueProvider = {
 	provide: Tokens.CONNECTION,
 	useValue: NODE_ENV === 'production'
 		? connection.prod
 		: connection.dev
 }
 
+
 @Module ({
-	providers: [ConnectionProvider]
+	providers: [ConnectionProvider],
+	exports: [/*ConnectionProvider*/Tokens.CONNECTION] // either one is valid
 })
 export class ConnectionModule {}
 
