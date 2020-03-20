3b2808d7b7dc7111e2fdcb5d30015ec49f65e70a
Gray Strider <zhoga.ivan@gmail.com>
2020-03-20 20:35:43 +1100

    jwt stragegy, guard

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, UseGuards, Post } from '@nestjs/common'
-import { LocalAuthGuard } from '@M/auth/local-auth.guard'
+import { Controller, UseGuards, Post, Get } from '@nestjs/common'
+import { LocalAuthGuard } from '@M/auth/guards/local-auth.guard'
 import { User } from '@/common/decorators/user.decorator'
 import { AuthService } from '@M/auth/auth.service'
+import { JwtAuthGuard } from '@M/auth/guards/jwt-auth.guard'
 
 @Controller ('auth')
 export class AuthController {
@@ -11,5 +12,11 @@ export class AuthController {
 	@Post ('login')
 	async login (@User () user: Express.User) {
 		return this.authService.login (user)
+	}
+	
+	@UseGuards (JwtAuthGuard)
+	@Get ('profile')
+	getProfile (@User () user: Express.User) {
+		return user
 	}
 }
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,22 +1,23 @@
 import { Module } from '@nestjs/common'
 import { UsersModule } from '@M/users/users.module'
 import { AuthService } from '@M/auth/auth.service'
-import { LocalStrategy } from '@M/auth/local.strategy'
+import { LocalStrategy } from '@M/auth/strategies/local.strategy'
 import { PassportModule } from '@nestjs/passport'
 import { AuthController } from '@M/auth/auth.controller'
 import { JwtModule } from '@nestjs/jwt'
 import { JWT_SECRET } from '@config'
+import { JwtStrategy } from '@M/auth/strategies/jwt.strategy'
 
 @Module ({
 	imports: [
 		UsersModule,
 		PassportModule,
-		JwtModule.register({
+		JwtModule.register ({
 			secret: JWT_SECRET,
-			signOptions: { expiresIn: '60s' },
+			signOptions: { expiresIn: '60s' }
 		})
 	],
-	providers: [AuthService, LocalStrategy],
+	providers: [AuthService, LocalStrategy, JwtStrategy],
 	controllers: [AuthController],
 	exports: [AuthService]
 })
