92012f6168c64acea84bb91b1e7b35ef5a250156
Gray Strider <zhoga.ivan@gmail.com>
2020-03-20 15:38:08 +1100

    getAllAndOverride in reflector, comments

--- src/common/guards/roles.guard.ts
+++ src/common/guards/roles.guard.ts
@@ -1,19 +1,33 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common'
 import { Reflector } from '@nestjs/core'
-import { Role } from '@/common/roles.type'
 import { intersection, isNonEmpty } from 'fp-ts/lib/Array'
 import { eqString, Eq } from 'fp-ts/lib/Eq'
 import { flow } from 'fp-ts/lib/function'
+import { sig } from '@qdev/utils-ts'
+import { CatsController } from '@M/cats/cats.controller'
 
-export const eqRole: Eq<Role> = eqString
+export const eqRole: Eq<Express.Role> = eqString
 export const hasRole = flow (intersection (eqRole), isNonEmpty)
 
 @Injectable ()
 export class RolesGuard implements CanActivate {
 	constructor (private readonly reflector: Reflector) {}
 	
+	/**
+	 * @param context
+	 * ExecutionContext extends ArgumentsHost, providing additional details about the current execution process.
+	 *
+	 */
 	canActivate (context: ExecutionContext): boolean {
-		const roles = this.reflector.get<Role[]> ('roles', context.getHandler ())
+		/**
+		 * @see CatsController
+		 * https://docs.nestjs.com/fundamentals/execution-context
+		 */
+		const roles = this.reflector.getAllAndOverride/*getAllAndMerge*/<Express.Role[]> ('roles', [
+			context.getHandler (),
+			context.getClass ()
+		])
+
 		if (!roles) {
 			return true
 		}
