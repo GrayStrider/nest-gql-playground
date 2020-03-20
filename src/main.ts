20667fd542bb2ed0efd39e33874e9566c6da2433
Gray Strider <zhoga.ivan@gmail.com>
2020-03-21 05:19:11 +1100

    bootstrap log

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import { NestFactory } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { HttpExceptionFilter } from '@/common/filters/http-exception.filter'
 import { TimeoutInterceptor } from '@/common/interceptors/timeout.interceptor'
-import { PORT, HOST } from '@config'
+import { PORT, HOST, NODE_ENV } from '@config'
 import { sig } from '@qdev/utils-ts'
 import { AppModule } from '@/app.module'
 
 
 async function bootstrap () {
+	if (!NODE_ENV) sig.error ('process.env is undefined!')
+	else sig.info (`Environment: ${NODE_ENV}`)
+	
 	const app = await NestFactory.create <NestExpressApplication> (AppModule)
 	app.useGlobalPipes (new ValidationPipe)
 	app.useGlobalFilters (new HttpExceptionFilter)
