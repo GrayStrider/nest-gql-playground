2608f9447f8045f86d7f8c05b655367a5e961e18
Gray Strider <zhoga.ivan@gmail.com>
2020-03-24 12:57:37 +1100

    simpllify

--- src/common/middleware/auth.middleware.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AuthMiddleware } from '@/common/middleware/auth.middleware'
-
-describe ('AuthMiddleware', () => {
-	it ('should be defined', () => {
-		expect (new AuthMiddleware ()).toBeDefined ()
-	})
-})
--- src/common/middleware/auth.middleware.ts
+++ src/common/middleware/auth.middleware.ts
@@ -1,12 +1,8 @@
-import { Injectable, NestMiddleware } from '@nestjs/common'
 import { Handler } from 'express'
 
-@Injectable ()
-export class AuthMiddleware implements NestMiddleware {
-	use: Handler = (req, res, next) => {
-		// validation steps here
-		req.user = { name: 'Ivan', roles: ['admin', 'user'], id: '1' }
-		next ()
-	}
+export const AuthMiddleware: Handler = (req, res, next) => {
+	// validation steps here
+	req.user = { name: 'Ivan', roles: ['admin', 'user'], id: '1' }
+	next ()
 }
 
