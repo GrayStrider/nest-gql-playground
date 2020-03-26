8633859987a7b5ba6d7c95ba3866f5e04da3b050
Gray Strider <zhoga.ivan@gmail.com>
2020-03-26 18:18:16 +1100

    extract

--- src/modules/KBF/KBF.module.ts
+++ src/modules/KBF/KBF.module.ts
@@ -1,26 +1,26 @@
 import { Module } from '@nestjs/common'
-import { GraphQLModule } from '@nestjs/graphql'
+import { GraphQLModule, GqlModuleOptions } from '@nestjs/graphql'
 import { KBFResolver } from '@M/KBF/KBF.resolver'
 import { CatFactsAPI } from '@M/cat-facts/cat-facts.datasource'
 import { formatError } from '@/graphql/apollo/formatError'
 
+const apolloOptions: GqlModuleOptions = {
+	autoSchemaFile: 'src/graphql/generated/schema.graphql',
+	dataSources: () => ({
+		catFacts: new CatFactsAPI ()
+	}),
+	playground: {
+		settings: {
+			'request.credentials': 'include'
+		}
+	},
+	formatError,
+	context: (args) => ({})
+}
+
 @Module ({
 	imports: [
-		GraphQLModule.forRoot ({
-			autoSchemaFile: 'src/graphql/generated/schema.graphql',
-			dataSources: () => ({
-				catFacts: new CatFactsAPI ()
-			}),
-			playground: {
-				settings: {
-					'request.credentials': 'include'
-				}
-			},
-			formatError,
-			context: (args) => ({
-			
-			})
-		})
+		GraphQLModule.forRoot (apolloOptions)
 	],
 	providers: [KBFResolver],
 	controllers: [],
