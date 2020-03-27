849e5946d9c4525d1d185adffbb42e57d8e67d06
Gray Strider <zhoga.ivan@gmail.com>
2020-03-28 01:56:06 +1100

    add validation

--- src/modules/KBF/inputs/task.input.ts
+++ src/modules/KBF/inputs/task.input.ts
@@ -1,38 +1,41 @@
 import { Field, InputType } from '@nestjs/graphql'
-import { MaxLength, IsOptional, MinLength, IsNotEmpty } from 'class-validator'
+import { MaxLength, IsOptional, IsNotEmpty } from 'class-validator'
 
 @InputType ()
 export class TaskInput {
-	@IsNotEmpty()
-	@MaxLength(50)
+	@IsNotEmpty ()
+	@MaxLength (50)
 	@Field ()
 	boardName: string
 	
-	@IsNotEmpty()
-	@MaxLength(100)
+	@IsNotEmpty ()
+	@MaxLength (100)
 	@Field ()
 	title: string
 	
 	@MaxLength (500)
+	@IsNotEmpty ()
 	@IsOptional ()
 	@Field ({ nullable: true })
 	description?: string
 	
-	@IsOptional()
+	@IsOptional ()
+	@MaxLength (20, { each: true })
+	@IsNotEmpty ({ each: true })
 	@Field (returns => [String], {
 		nullable: true
 	})
 	tags?: string[]
 	
-	@IsOptional()
+	@IsOptional ()
 	@Field ({ nullable: true })
 	colorName?: string
 	
-	@IsOptional()
+	@IsOptional ()
 	@Field ({ nullable: true })
 	columnName?: string
 	
-	@IsOptional()
+	@IsOptional ()
 	@Field ({ nullable: true })
 	swimlaneName?: string
 	
--- src/modules/KBF/inputs/task.search.input.ts
+++ src/modules/KBF/inputs/task.search.input.ts
@@ -1,23 +1,36 @@
 import { ArgsType, Field } from '@nestjs/graphql'
-import { Max } from 'class-validator'
+import { Max, IsNotEmpty, MaxLength, IsOptional, IsBoolean } from 'class-validator'
 
 @ArgsType ()
 export class TaskSearchInput {
-	
+	@IsOptional()
+	@IsNotEmpty()
+	@MaxLength(100)
 	@Field ({ nullable: true })
 	title: string
 	
+	@IsOptional()
+	@IsNotEmpty()
+	@MaxLength(500)
 	@Field ({ nullable: true })
 	description: string
 	
+	@IsOptional()
+	@IsNotEmpty()
+	@MaxLength(100)
 	@Field ({ nullable: true })
 	id: string
 	
+	@IsOptional()
+	@IsBoolean()
 	@Field ({ nullable: true })
 	completed: boolean
 	
+	@IsNotEmpty ({ each: true })
+	@MaxLength (20, { each: true })
+	@IsOptional ()
 	@Field (returns => [String], { nullable: true })
-	tag: string
+	tag: string[]
 	
 }
 
