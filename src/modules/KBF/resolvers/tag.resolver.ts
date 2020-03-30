8eb2e4b672d00a3355ca394667995b4f7c3faca5
Gray Strider <zhoga.ivan@gmail.com>
2020-03-30 22:04:47 +1100

    tag resolver

--- src/modules/KBF/resolvers/tag.resolver.ts
+++ src/modules/KBF/resolvers/tag.resolver.ts
@@ -1,6 +1,11 @@
-import { Resolver, Query, Args } from '@nestjs/graphql'
+import { Resolver, Query, Args, Mutation } from '@nestjs/graphql'
 import { Tag } from '@M/KBF/entity/Tag'
 import { TagInput } from '@M/KBF/inputs/tag.input'
+import { Task } from '@M/KBF/entity/Task'
+import { without, isEmpty } from 'ramda'
+import { NotFoundByIDError, ErrorCodes2 } from '@/common/errors'
+import { Board } from '@M/KBF/entity/Board'
+import { ApolloError } from 'apollo-server-errors'
 
 @Resolver ()
 export class TagResolver {
@@ -8,5 +13,36 @@ export class TagResolver {
 	async tags (@Args ('searchBy', { nullable: true })
 		            {}: TagInput): Promise<Tag[]> {
 		return Tag.find ()
+	}
+	
+	@Mutation (returns => Tag)
+	async addTag (@Args ('data')
+		              { tasksIDs, boardName, name, ...rest }: TagInput): Promise<Tag> {
+		const board = await Board.findOne ({ name: boardName })
+		if (!board) {
+			throw new ApolloError (`Board <${boardName}> not found`, ErrorCodes2.NOT_FOUND,
+				{
+					requestedName: boardName
+				})
+		}
+		
+		const exists = await Tag.find ({ name, board })
+		if (!isEmpty (exists)) {
+			throw new ApolloError (`Tag with the name <${name}> already exists: <${exists[0].id}>`,
+				ErrorCodes2.NOT_UNIQUE,
+				{ existingEntityID: exists[0].id })
+		}
+		
+		if (tasksIDs) {
+			const tasks = await Task.findByIds (tasksIDs)
+			const notFound = without (tasks.map (t => t.id), tasksIDs)
+			if (!isEmpty (notFound)) {
+				throw NotFoundByIDError ('task', notFound[0])
+			}
+			return Tag.create ({ name, board, tasks, ...rest }).save ()
+		}
+		
+		return Tag.create ({ name, board, ...rest }).save ()
+		
 	}
 }
