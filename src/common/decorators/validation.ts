0df7d4869b271f7b7fe7d1326b497bf27f648d8b
Gray Strider <zhoga.ivan@gmail.com>
2020-03-30 13:27:51 +1100

    Replace validation constants in favor of value constants, shared with database constraints

--- src/common/decorators/validation.ts
+++ src/common/decorators/validation.ts
@@ -1,22 +1,20 @@
 import { ReturnTypeFuncValue, Field } from '@nestjs/graphql'
 import { composeFieldDecorators } from '@qdev/utils-ts'
-import { IsOptional, MaxLength, IsNotEmpty, Max, IsInt } from 'class-validator'
+import { IsOptional, MaxLength, IsNotEmpty, Max, IsInt, Min } from 'class-validator'
 
 
 export const FieldNullable = (returns?: ReturnTypeFuncValue) => returns
-	? composeFieldDecorators (Field (returns_ => returns, { nullable: true }), IsOptional ())
-	: composeFieldDecorators (Field ({ nullable: true }), IsOptional ())
+	? composeFieldDecorators (
+		Field (returns_ => returns, { nullable: true }),
+		IsOptional ())
+	: composeFieldDecorators (
+		Field ({ nullable: true }),
+		IsOptional ())
 
-const ValidString = (maxLength: number) => composeFieldDecorators (
+export const ValidString = (maxLength: number) => composeFieldDecorators (
 	MaxLength (maxLength), IsNotEmpty ()
 )
 
-export const String20 = ValidString (20)
-export const String50 = ValidString (50)
-export const String100 = ValidString (100)
-export const String5K = ValidString (5000)
-
-const ValidNumber = (max: number) => composeFieldDecorators (
-	IsInt (), Max (max)
+export const ValidNumber = (max: number, min = 0) => composeFieldDecorators (
+	IsInt (), Max (max), Min (min)
 )
-export const Number500 = ValidNumber (500)
