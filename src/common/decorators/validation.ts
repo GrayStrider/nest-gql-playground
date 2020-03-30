7515b72384e7f3c9e52907db89fb45cec25a0b36
Gray Strider <zhoga.ivan@gmail.com>
2020-03-30 23:32:04 +1100

    add options

--- src/common/decorators/validation.ts
+++ src/common/decorators/validation.ts
@@ -1,14 +1,14 @@
-import { ReturnTypeFuncValue, Field } from '@nestjs/graphql'
+import { ReturnTypeFuncValue, Field, FieldOptions } from '@nestjs/graphql'
 import { composeFieldDecorators } from '@qdev/utils-ts'
 import { IsOptional, MaxLength, IsNotEmpty, Max, IsInt, Min, ArrayNotEmpty } from 'class-validator'
 
 
-export const FieldNullable = (returns?: ReturnTypeFuncValue) => returns
+export const FieldNullable = (returns?: ReturnTypeFuncValue, options?: FieldOptions) => returns
 	? composeFieldDecorators (
-		Field (returns_ => returns, { nullable: true }),
+		Field (returns_ => returns, { nullable: true, ...options }),
 		IsOptional ())
 	: composeFieldDecorators (
-		Field ({ nullable: true }),
+		Field ({ nullable: true, ...options }),
 		IsOptional ())
 
 export const ValidString = (maxLength: number, each = false) => composeFieldDecorators (
