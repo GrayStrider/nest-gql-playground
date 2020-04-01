af4770dffc0d53bc8fc979e5e56ca29013b1baf9
Gray Strider <zhoga.ivan@gmail.com>
2020-04-02 01:07:31 +1100

    del export

--- src/common/middleware/auth.middleware.example.ts
+++ src/common/middleware/auth.middleware.example.ts
@@ -1,6 +1,6 @@
 import { Handler } from 'express'
 
-export const AuthMiddleware: Handler = (req, res, next) => {
+const AuthMiddleware: Handler = (req, res, next) => {
 	// validation steps here
 	req.user = { name: 'Ivan', roles: ['admin', 'user'], id: '1' }
 	next ()
