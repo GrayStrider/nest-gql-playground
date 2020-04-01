c2de1567df1df662c718df49c5b213c9f18c5166
Gray Strider <zhoga.ivan@gmail.com>
2020-04-02 00:49:00 +1100

    update consts

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,12 +1,5 @@
-import IORedis from 'ioredis'
 import redis from 'redis'
 import { RedisPubSub as type } from 'graphql-redis-subscriptions'
-
-export enum Tokens {
-	CONNECTION = 'CONNECTION',
-	ASYNC_CONNECTION = 'ASYNC_CONNECTION',
-	CONFIG_OPTIONS = 'CONFIG_OPTIONS'
-}
 
 export enum REDIS {
 	SESSION = 'SESSION',
