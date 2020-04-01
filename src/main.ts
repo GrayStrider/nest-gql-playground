5cb775332def4b69ee570829e4b12d3239da58a8
Gray Strider <zhoga.ivan@gmail.com>
2020-04-02 00:44:00 +1100

    error on undefined NODE_ENV

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,14 @@
-import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
-import { NestExpressApplication } from '@nestjs/platform-express'
-import { HttpExceptionFilter } from '@/common/filters/http-exception.filter'
-import { TimeoutInterceptor } from '@/common/interceptors/timeout.interceptor'
 import { PORT, HOST, NODE_ENV } from '@config'
-import { sig } from '@qdev/utils-ts'
-import { AppModule } from '@M/app/app.module'
+import { sig, toDefault } from '@qdev/utils-ts'
 import { KBFModule } from '@M/KBF/KBF.module'
 
 
 async function bootstrap () {
-	if (!NODE_ENV) sig.error ('process.env is undefined!')
-	else sig.info (`Environment: ${NODE_ENV}`)
-	const app = await NestFactory.create <NestExpressApplication> (KBFModule)
+	toDefault (NODE_ENV,
+		new Error ('process.env is undefined!'))
+	sig.info (`Environment: ${NODE_ENV}`)
+	const app = await NestFactory.create (KBFModule)
 	await app.listen (PORT)
 	sig.success (`Application is running on: http://${HOST}:${PORT}`)
 }
