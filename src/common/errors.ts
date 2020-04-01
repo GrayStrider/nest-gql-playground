f7d160b3abcd73437ed1c2fdb4f00d835be979b8
Gray Strider <zhoga.ivan@gmail.com>
2020-04-02 00:52:51 +1100

    cleanup

--- src/common/errors.ts
+++ src/common/errors.ts
@@ -7,8 +7,38 @@ interface IExpectedError {
 	details?: AnyObject | string
 }
 
+export enum ErrorCodes {
+	'VALIDATION_ERROR' = 'VALIDATION_ERROR',
+	'NOT_FOUND' = 'NOT_FOUND',
+	'UNATHORIZED' = 'UNATHORIZED',
+	LIMIT_REACHED = 'LIMIT_REACHED',
+	NOT_UNIQUE = 'NOT_UNIQUE'
+}
+
+const Errors = {
+	Validation: makeCustomError (ErrorCodes.VALIDATION_ERROR, 'Validation error'),
+	NotFound: makeCustomError (ErrorCodes.NOT_FOUND, 'Object not found'),
+	Unathorized: makeCustomError (ErrorCodes.UNATHORIZED, 'Unathorized to perform requested action'),
+	InvalidCredentials: makeCustomError (ErrorCodes.UNATHORIZED, 'Invalid credentials provided'),
+	NotUnique: makeCustomError (ErrorCodes.NOT_UNIQUE,
+		'Unique constraint violation')
+}
+
+const userNotFoundError = (id: string) =>
+	new Errors.NotFound (`User ${truncate (id, { length: 10 })} not found`)
+
+const NotFoundByIDError = (name: string, id: string) =>
+	new ApolloError (`${capitalize (name)} ${truncate (id, {
+			length: 10
+		})} was not found`,
+		ErrorCodes.NOT_FOUND, {
+			providedID: id
+		})
+
+
 function makeCustomError (
-	code: ErrorCodes, defaultMessage: string, defaultDetails?: AnyObject | string
+	code: ErrorCodes, defaultMessage: string,
+	defaultDetails?: AnyObject | string
 ) {
 	
 	return class ExpectedError extends ApolloError implements IExpectedError {
@@ -26,33 +56,10 @@ function makeCustomError (
 	
 }
 
-export enum ErrorCodes {
-	'VALIDATION_ERROR' = 'VALIDATION_ERROR',
-	'NOT_FOUND' = 'NOT_FOUND',
-	'UNATHORIZED' = 'UNATHORIZED',
-	LIMIT_REACHED = 'LIMIT_REACHED',
-	NOT_UNIQUE = 'NOT_UNIQUE'
-}
-
-const Errors = {
-	Validation: makeCustomError (ErrorCodes.VALIDATION_ERROR, 'Validation error'),
-	NotFound: makeCustomError (ErrorCodes.NOT_FOUND, 'Object not found'),
-	Unathorized: makeCustomError (ErrorCodes.UNATHORIZED, 'Unathorized to perform requested action'),
-	InvalidCredentials: makeCustomError (ErrorCodes.UNATHORIZED, 'Invalid credentials provided'),
-	NotUnique: makeCustomError(ErrorCodes.NOT_UNIQUE,
-		'Unique constraint violation')
-}
-
-const userNotFoundError = (id: string) =>
-	new Errors.NotFound (`User ${truncate (id, { length: 10 })} not found`)
-
 export default Errors
-
-export const NotFoundByIDError = (name: string, id: string) => new ApolloError (`${capitalize(name)} ${truncate (id, {
-		length: 10
-	})} was not found`,
-	ErrorCodes.NOT_FOUND, {
-		providedID: id
-	})
-
-export {  userNotFoundError, makeCustomError, IExpectedError }
+export {
+	userNotFoundError,
+	makeCustomError,
+	IExpectedError,
+	NotFoundByIDError
+}
