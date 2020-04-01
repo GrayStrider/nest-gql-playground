ee4c35b176500b67a0752a3749c9f235f29f571a
Gray Strider <zhoga.ivan@gmail.com>
2020-04-02 00:25:38 +1100

    remove comment

--- src/common/guards/roles.guard.ts
+++ src/common/guards/roles.guard.ts
@@ -3,8 +3,6 @@ import { Reflector } from '@nestjs/core'
 import { intersection, isNonEmpty } from 'fp-ts/lib/Array'
 import { eqString, Eq } from 'fp-ts/lib/Eq'
 import { flow } from 'fp-ts/lib/function'
-import { sig } from '@qdev/utils-ts'
-import { CatsController } from '@M/cats/cats.controller'
 
 export const eqRole: Eq<Express.Role> = eqString
 export const hasRole = flow (intersection (eqRole), isNonEmpty)
@@ -12,22 +10,14 @@ export const hasRole = flow (intersection (eqRole), isNonEmpty)
 @Injectable ()
 export class RolesGuard implements CanActivate {
 	constructor (private readonly reflector: Reflector) {}
-	
-	/**
-	 * @param context
-	 * ExecutionContext extends ArgumentsHost, providing additional details about the current execution process.
-	 *
-	 */
+
 	canActivate (context: ExecutionContext): boolean {
-		/**
-		 * @see CatsController
-		 * https://docs.nestjs.com/fundamentals/execution-context
-		 */
+		// use SetMetadata or custom decorators to set metadata
 		const roles = this.reflector.getAllAndOverride/*getAllAndMerge*/<Express.Role[]> ('roles', [
 			context.getHandler (),
 			context.getClass ()
 		])
-
+		
 		if (!roles) {
 			return true
 		}
