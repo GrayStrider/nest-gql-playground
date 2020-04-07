375b6c4f8bb07dc6496f3dbd3f6bd01d9ac814ff
Gray Strider <zhoga.ivan@gmail.com>
2020-04-08 10:26:46 +1100

    add boardMember class

--- src/modules/kanban/entity/Tag.ts
+++ src/modules/kanban/entity/Tag.ts
@@ -1,19 +1,15 @@
-import { Column, ManyToMany, ManyToOne, Unique } from 'typeorm'
+import { Column, ManyToMany, Unique } from 'typeorm'
 import { Field } from '@nestjs/graphql'
 import { Task } from '@M/kanban/entity/Task'
-import { Base } from '@M/kanban/entity/_Base'
 import { EntityObject } from '@/common/decorators/entity-object.decorator'
-import { Board } from '@M/kanban/entity/Board'
+import { boardMember } from '@M/kanban/entity/_BoardMember'
 
 export const nameLength = 20
 export const descriptionLength = 100
 
 @EntityObject
 @Unique (['name', 'board'])
-export class Tag extends Base {
-	@ManyToOne (type => Board,
-		board => board.tags)
-	board: Board
+export class Tag extends boardMember ('tags') {
 	
 	@Column ({ length: nameLength })
 	@Field ()
---
+++ src/modules/kanban/entity/_BoardMember.ts
@@ -0,0 +1,16 @@
+import { ObjectType } from '@nestjs/graphql'
+import { ManyToOne } from 'typeorm'
+import { Base } from '@M/kanban/entity/_Base'
+import { Board } from '@M/kanban/entity/Board'
+
+// ManyToOne
+export function boardMember (reverseKey: keyof Board) {
+	@ObjectType (`Board_${reverseKey}`, { isAbstract: true })
+	abstract class BoardMember extends Base {
+		@ManyToOne (type => Board,
+			board => board[reverseKey])
+		board: Board
+	}
+	
+	return BoardMember
+}
